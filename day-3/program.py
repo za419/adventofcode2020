aa76ee9bb409895389e96b0367462cff8104e5df
Ryan Hodin <ryan951357@gmail.com>
2020-12-03 19:11:02 -0600

    Solve part 2
    This was super easy thanks to how I wrote part 1, honestly.

--- day-3/program.py
+++ day-3/program.py
@@ -9,16 +9,26 @@ with open('input', 'r') as file:
 # Trim each line
 values=[value.strip() for value in values]
 
+# Shared helper
+def countTrees(inc_x, inc_y):
+    count=0
+    x_index=0
+    for y_index in range(inc_y, len(values), inc_y):
+        row=values[y_index]
+        # Increment with wraparound
+        x_index+=inc_x
+        x_index%=len(row)
+        if row[x_index]=='#':
+            count+=1
+    return count
+
 # Part 1
-inc_x=3
-inc_y=1
-count=0
-x_index=0
-for y_index in range(inc_y, len(values), inc_y):
-    row=values[y_index]
-    # Increment with wraparound
-    x_index+=inc_x
-    x_index%=len(row)
-    if row[x_index]=='#':
-        count+=1
-print('Part One: Hit', count, 'trees')
+print('Part One: Hit', countTrees(3,1), 'trees')
+
+# Part 2
+result = countTrees(1,1)
+result*= countTrees(3,1)
+result*= countTrees(5,1)
+result*= countTrees(7,1)
+result*= countTrees(1,2)
+print("Part Two:", result, "trees^5")
