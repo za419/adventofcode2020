cd37a95231ac1e5145ea219767207c0864123477
Ryan Hodin <ryan951357@gmail.com>
2020-12-06 00:33:16 -0600

    Implement first pass at seatID calculator
    I'm going to generalize this partition algorithm to make this a little
    nicer

--- day-5/program.py
+++ day-5/program.py
@@ -8,3 +8,38 @@ with open('input', 'r') as file:
     
 # Trim each line
 values=[value.strip() for value in values]
+
+# Part One: Decode some rather insane boarding passes
+# Like seriously, what's wrong with '35C'? IT WORKS GOD DAMN YOU
+def calcSeatID(seat):
+    # First, do the row
+    row=0
+    minimum=0
+    maximum=127
+    i=0
+    while minimum<maximum:
+        if seat[i]=='F':
+            maximum=minimum+(maximum-minimum)//2
+        elif seat[i]=='B':
+            minimum=maximum-(maximum-minimum)//2
+        else:
+            raise ValueError('Unable to resolve seat number')
+        i+=1
+    row=minimum
+
+    # Now the column
+    column=0
+    minimum=0
+    maximum=7
+    while minimum<maximum:
+        print(minimum,maximum,i)
+        if seat[i]=='L':
+            maximum=minimum+(maximum-minimum)//2
+        elif seat[i]=='R':
+            minimum=maximum-(maximum-minimum)//2
+        else:
+            raise ValueError('Unable to resolve seat number')
+        i+=1
+    column=minimum
+    
+    return row*8 + column
